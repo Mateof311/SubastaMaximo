ad66a6845f6939ceb978d3ac9f0b351c0fe74344
Mateo Fux <106816941+Mateof311@users.noreply.github.com>
2025-02-14 16:03:28 -0300

    EditData

--- src/Subasta.jsx
+++ src/Subasta.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import ObjetoSubasta from "./ObjetoSubasta";
 import "./Subasta.css";
-import BatidoraPeabody from "../src/Img/BatidoraPeabody.jpg";
+
 import Heladera from "../src/Img/Heladera.jpg";
 import Proyector from "../src/Img/Proyector.jpg";
 import SillonNegro from "../src/Img/SillonNegro.jpg";
 import TvSamsung from "../src/Img/TvSamsung.jpg";
 import Somier from "../src/Img/Colchon2Plazas.jpg";
 import Colchon1Plaza from "../src/Img/Colchon1Plaza.jpg";
-import ImagenVacia from "../src/Img/ImagenVacia.jpg";
 import Horno from "../src/Img/HornoElectrico.jpg";
+import ImagenVacia from "../src/Img/ImagenVacia.jpg";
+import BatidoraPeabody from "../src/Img/BatidoraPeabody.jpg";
 import Moto from "../src/Img/Moto.jpg";
 
 const objetosSubasta = [
     { id: "1", nombre: "TV Samsung 50'' UHD 4K + Soporte TV Brazo Tagwood", imagen: TvSamsung, precioBase: 399990 },
-    { id: "2", nombre: "Batidora Peabody Planetaria", imagen: BatidoraPeabody, precioBase: 149990 },
-    { id: "3", nombre: "Heladera No Frost Patrick 285Lt", imagen: Heladera, precioBase: 349990 },
-    { id: "4", nombre: "Proyector Gadnic Unique 2500 Lum", imagen: Proyector, precioBase: 69990 },
-    { id: "5", nombre: "Sillón Cuerina 3 Cuerpos", imagen: SillonNegro, precioBase: 99990 },
-    { id: "6", nombre: "Colchon 1 Plaza Inducol", imagen: Colchon1Plaza, precioBase: 39990 },
-    { id: "7", nombre: "Sommier Piero Modena Doble Pillow Top 140 x 190 cm", imagen: Somier, precioBase: 199990 },
-    { id: "8", nombre: "Horno Electrico Liliana Facilcook", imagen: Horno, precioBase: 59990 },
-    //{ id: "8", nombre: "Zanella Styler 150", imagen: Moto, precioBase: 649990 },
+    { id: "2", nombre: "Heladera No Frost Patrick 285Lt", imagen: Heladera, precioBase: 349990 },
+    { id: "3", nombre: "Proyector Gadnic Unique 2500 Lum", imagen: Proyector, precioBase: 69990 },
+    { id: "4", nombre: "Sillón Cuerina 3 Cuerpos", imagen: SillonNegro, precioBase: 99990 },
+    { id: "5", nombre: "Colchon 1 Plaza Inducol", imagen: Colchon1Plaza, precioBase: 39990 },
+    { id: "6", nombre: "Sommier Piero Modena Doble Pillow Top 140 x 190 cm", imagen: Somier, precioBase: 199990 },
+    { id: "7", nombre: "Horno Electrico Liliana Facilcook", imagen: Horno, precioBase: 59990 },
+    //{ id: "8", nombre: "Batidora Peabody Planetaria", imagen: BatidoraPeabody, precioBase: 149990 },
+    //{ id: "9", nombre: "Zanella Styler 150", imagen: Moto, precioBase: 649990 },
 ];
 
 function Subasta() {
